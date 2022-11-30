485810ae182884ba67e76b9c91929073de9bd3ec
Dmitriy Danilin <danilindn@icloud.com>
2022-11-30 19:19:38 +0300

    create test for update FirstAidKit in CoreData

--- MedicinesV2Tests/ServiceLayer/CoreDataServiceTests.swift
+++ MedicinesV2Tests/ServiceLayer/CoreDataServiceTests.swift
@@ -34,8 +34,19 @@ final class CoreDataServiceTests: XCTestCase {
         }
         
         // Then
-        let exp = expectation(description: "Ожидание чтения данных из базы")
-        var sut: [DBFirstAidKit] = []
+        let sut = makeSut()
+        XCTAssertEqual(sut?.title, "TestKit")
+    }
+    
+    func test_updateFirstAidKit() {
+        // Given
+        let testKitName = "TestKit"
+        let editKitName = "EditTestKit"
+        coreDataService.performSave { context in
+            self.coreDataService.create(testKitName, context: context)
+        }
+        
+        // When
         coreDataService.performSave { context in
             self.coreDataService.fetch(
                 DBFirstAidKit.self,
@@ -43,16 +54,28 @@ final class CoreDataServiceTests: XCTestCase {
                 completion: { result in
                     switch result {
                     case .success(let firstAidKits):
-                        sut = firstAidKits
-                        exp.fulfill()
+                        guard let firstAidKit = firstAidKits.first else {
+                            return XCTFail("Данные не найдены")
+                        }
+                        
+                        if firstAidKit.title == nil {
+                            XCTFail("Имя не задано")
+                        }
+                        
+                        self.coreDataService.update(
+                            firstAidKit,
+                            newName: editKitName,
+                            context: context
+                        )
                     case .failure(let error):
                         XCTFail(error.localizedDescription)
                     }
                 })
         }
-        wait(for: [exp], timeout: 3)
         
-        XCTAssertEqual(sut.first?.title, "TestKit")
+        // Then
+        let sut = makeSut()
+        XCTAssertEqual(sut?.title, editKitName)
     }
     
     func test_deleteFirstAidKit() {
@@ -86,25 +109,8 @@ final class CoreDataServiceTests: XCTestCase {
         }
         
         // Then
-        var sut: [DBFirstAidKit] = []
-        let expRead = expectation(description: "Ожидание чтения данных из базы")
-        coreDataService.performSave { context in
-            self.coreDataService.fetch(
-                DBFirstAidKit.self,
-                from: context,
-                completion: { result in
-                    switch result {
-                    case .success(let firstAidKits):
-                        sut = firstAidKits
-                        expRead.fulfill()
-                    case .failure(let error):
-                        XCTFail(error.localizedDescription)
-                    }
-                })
-        }
-        wait(for: [expRead], timeout: 3)
-        
-        XCTAssertNil(sut.first)
+        let sut = makeSut()
+        XCTAssertNil(sut)
     }
     
     private func clearAllData() {
@@ -123,6 +129,29 @@ final class CoreDataServiceTests: XCTestCase {
                     }
                 })
         })
+    }
+    
+    private func makeSut() -> DBFirstAidKit? {
+        var sut: [DBFirstAidKit] = []
+        
+        let expRead = expectation(description: "Ожидание чтения данных из базы")
+        coreDataService.performSave { context in
+            self.coreDataService.fetch(
+                DBFirstAidKit.self,
+                from: context,
+                completion: { result in
+                    switch result {
+                    case .success(let firstAidKits):
+                        sut = firstAidKits
+                        expRead.fulfill()
+                    case .failure(let error):
+                        XCTFail(error.localizedDescription)
+                    }
+                })
+        }
+        wait(for: [expRead], timeout: 3)
+        
+        return sut.first
     }
 
 }
